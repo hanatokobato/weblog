334b351671f3f3d4b250acfe12f3011f83f3b900
bangld <kobato2096@gmail.com>
2017-09-07 07:38:51 +0700

    Following users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: %i(index show)
-  before_action :load_user, only: :show
+  before_action :load_user, only: %i(show following followers)
 
   def index
     @users = User.select(:id, :name, :email).order(:name)
@@ -10,6 +10,18 @@ class UsersController < ApplicationController
   def show
     @posts = @user.posts.order_latest
       .page(params[:page]).per Settings.post.per_page
+  end
+
+  def following
+    @title = t ".following"
+    @users = @user.following.page(params[:page]).per Settings.users.per_page
+    render "show_follow"
+  end
+
+  def followers
+    @title = t ".followers"
+    @users = @user.followers.page(params[:page]).per Settings.users.per_page
+    render "show_follow"
   end
 
   private
