5b5e111d01160dcbd19ae669babffd7a2b7b3cb9
bangld <kobato2096@gmail.com>
2017-09-07 07:38:51 +0700

    static-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root "static_pages#show", page_type: "home"
+
+  get "static_pages/*page_type", to: "static_pages#show", as: "static_pages"
+
   devise_for :users
 end
