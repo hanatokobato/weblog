26f17dc1fd584261757a1e041d4b56e3a9a89cae
bangld <kobato2096@gmail.com>
2017-09-07 07:38:51 +0700

    signup-login-logout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery.turbolinks
 //= require rails-ujs
+//= require bootstrap
 //= require_tree .
