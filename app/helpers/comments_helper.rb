2a2ccdad7836e14f171f2a93bd68d200c30abf41
bangld <kobato2096@gmail.com>
2017-09-11 09:06:50 +0700

    make-comment-on-post

---
+++ app/helpers/comments_helper.rb
@@ -0,0 +1,14 @@
+module CommentsHelper
+  def commentable_type commentable
+    commentable.class.name.downcase
+  end
+
+  def can_edit_comment? comment
+    user_signed_in? && current_user.is_user?(comment.user)
+  end
+
+  def can_delete_comment? comment, post
+    user_signed_in? &&
+      (current_user.is_user?(comment.user) || current_user.is_user?(post.user))
+  end
+end
